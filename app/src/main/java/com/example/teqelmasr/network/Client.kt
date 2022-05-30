9c037a6aec3f5250d7dfe5e5456e89fa79ff9848
Mariam Elkirch <mariamk019@gmail.com>
2022-05-30 22:22:03 +0200

    creation Remote

--- app/src/main/java/com/example/teqelmasr/network/Client.kt
+++ app/src/main/java/com/example/teqelmasr/network/Client.kt
@@ -1,6 +1,10 @@
 package com.example.teqelmasr.network
 
 class Client : RemoteSource {
+    /*override suspend fun getCurrentWeather(units: String, lat: String, lng: String, lang: String): WeatherResponse {
+        val weatherService = RetrofitHelper.getInstance().create(WeatherService::class.java)
+        return weatherService.getCurrentWeather(units, lat, lng, lang)
+    }*/
      companion object{
             private var instance: Client? = null
             fun getInstance(): Client{
