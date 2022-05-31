ae24c43160808a3bcaada6ba2e58ea97c23d6542
Mariam Elkirch <mariamk019@gmail.com>
2022-05-31 18:01:39 +0200

    add equipment

--- app/src/main/java/com/example/teqelmasr/network/Client.kt
+++ app/src/main/java/com/example/teqelmasr/network/Client.kt
@@ -1,15 +1,23 @@
 package com.example.teqelmasr.network
 
+import com.example.teqelmasr.model.ProductItem
+import retrofit2.Response
+
 class Client : RemoteSource {
     /*override suspend fun getCurrentWeather(units: String, lat: String, lng: String, lang: String): WeatherResponse {
         val weatherService = RetrofitHelper.getInstance().create(WeatherService::class.java)
         return weatherService.getCurrentWeather(units, lat, lng, lang)
     }*/
+
      companion object{
             private var instance: Client? = null
             fun getInstance(): Client{
                 return  instance?: Client()
             }
         }
+
+    override suspend fun storeProduct(product: ProductItem): Response<ProductItem> {
+        TODO("Not yet implemented")
+    }
 
 }
--- app/src/main/java/com/example/teqelmasr/network/RemoteSource.kt
+++ app/src/main/java/com/example/teqelmasr/network/RemoteSource.kt
@@ -1,4 +1,9 @@
 package com.example.teqelmasr.network
 
+import com.example.teqelmasr.model.ProductItem
+import retrofit2.Response
+import retrofit2.http.Body
+
 interface RemoteSource {
+    suspend fun storeProduct(@Body product: ProductItem): Response<ProductItem>
 }
