a1072edf1615b3888648362b300193ef4aca1016
Rawan_Ashraf <rbahnasy98@gmail.com>
2022-06-07 17:24:43 +0200

    change search with filterable

--- app/src/main/java/com/example/teqelmasr/displayEquipmentSell/view/OnProductClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.teqelmasr.displayEquipmentSell.view
-
-import com.example.teqelmasr.model.Product
-
-interface OnProductClickListener {
-    fun onProductClick(product: Product)
-}
