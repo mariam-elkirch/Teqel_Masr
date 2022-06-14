e33a0fc845b5904e2f092c3f182573ca0e3d970f
mohamedkh242 <mohamedkholif710@gmail.com>
2022-06-14 21:33:31 +0200

    show snack bar on offer product click if user in not logged in

--- app/src/main/java/com/example/teqelmasr/MainActivity.kt
+++ app/src/main/java/com/example/teqelmasr/MainActivity.kt
@@ -5,15 +5,18 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.teqelmasr.home.HomeActivity
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("TAG", "Auth: ${FirebaseAuth.getInstance().currentUser?.uid}")
         //supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FFFFFF")))
         Handler().postDelayed({
             val intent = Intent(this, HomeActivity::class.java)
--- app/src/main/java/com/example/teqelmasr/home/HomeFragment.kt
+++ app/src/main/java/com/example/teqelmasr/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.teqelmasr.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,10 +10,13 @@ import androidx.navigation.NavDirections
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.teqelmasr.R
+import com.example.teqelmasr.authentication.login.LoginActivity
 import com.example.teqelmasr.databinding.FragmentDisplayEquipmentSellBinding
 import com.example.teqelmasr.databinding.FragmentHomeBinding
 import com.example.teqelmasr.displaySparePart.view.SparePartsFilterBottomSheetFragmentDirections
 import com.example.teqelmasr.model.Product
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeFragment : Fragment() {
 
@@ -21,25 +25,26 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-     // binding = FragmentHomeBinding.inflate(inflater, container, false)
+        // binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        return  binding.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.equipmentSellBtn.setOnClickListener {
-           // binding.root.findNavController().navigate(R.id.action_homeFragment_to_displayEquipmentSellFragment)
+            // binding.root.findNavController().navigate(R.id.action_homeFragment_to_displayEquipmentSellFragment)
             val action =
                 HomeFragmentDirections.actionHomeFragmentToDisplayEquipmentSellFragment(null)
             findNavController().navigate(action)
         }
         binding.equipmentRentBtn.setOnClickListener {
-       //     binding.root.findNavController().navigate(R.id.action_homeFragment_to_displayEquipmentRentFragment)
-            val action: NavDirections = HomeFragmentDirections.actionHomeFragmentToDisplayEquipmentRentFragment(
-                null
-            )
-          findNavController().navigate(action)
+            //     binding.root.findNavController().navigate(R.id.action_homeFragment_to_displayEquipmentRentFragment)
+            val action: NavDirections =
+                HomeFragmentDirections.actionHomeFragmentToDisplayEquipmentRentFragment(
+                    null
+                )
+            findNavController().navigate(action)
         }
         binding.SpareBtn.setOnClickListener {
             val action =
@@ -48,10 +53,23 @@ class HomeFragment : Fragment() {
 
         }
         binding.sellertn.setOnClickListener {
-            val action: NavDirections = HomeFragmentDirections.actionHomeFragmentToAddEquipmentSellFragment(null)
-            binding.root.findNavController().navigate(action)
-        }
+            if (FirebaseAuth.getInstance().currentUser?.uid.isNullOrEmpty()) {
+                Snackbar.make(
+                    binding.root,
+                    getString(R.string.have_to_login),
+                    Snackbar.LENGTH_INDEFINITE
+                ).setAction(getString(R.string.login)) {
+                    startActivity(Intent(requireContext(), LoginActivity::class.java))
+                }.setDuration(6000).show()
 
+            } else {
+                val action: NavDirections =
+                    HomeFragmentDirections.actionHomeFragmentToAddEquipmentSellFragment(null)
+                binding.root.findNavController().navigate(action)
+            }
+
+
+        }
 
 
     }
