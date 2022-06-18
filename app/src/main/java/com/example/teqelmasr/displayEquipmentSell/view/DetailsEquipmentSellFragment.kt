d958aa9d13ac9ddde6663cdd6343744aa67b072b
Mariam Elkirch <mariamk019@gmail.com>
2022-06-18 16:37:14 +0200

    contact info screen

--- app/src/main/java/com/example/teqelmasr/displayEquipmentSell/view/DetailsEquipmentSellFragment.kt
+++ app/src/main/java/com/example/teqelmasr/displayEquipmentSell/view/DetailsEquipmentSellFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.teqelmasr.R
+import com.example.teqelmasr.addEquipmentSell.view.AddEquipmentSellFragmentDirections
 import com.example.teqelmasr.displayEquipmentRent.view.DetailsEquipmentRentFragmentArgs
+import com.example.teqelmasr.model.ContactInfo
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -40,8 +45,12 @@ class DetailsEquipmentSellFragment : Fragment() {
         var price: TextView = view.findViewById(R.id.price_txt)
         price.text= args.productsell.variants?.get(0)?.price.toString()
         var date: TextView = view.findViewById(R.id.date_txt)
-
-
+        var show: Button = view.findViewById(R.id.showButton)
+        show.setOnClickListener{
+            var contact = ContactInfo(args.productsell.variants?.get(0)?.option1)
+            val action = DetailsEquipmentSellFragmentDirections.actionDetailsEquipmentSellFragmentToContactInfoFragment(contact)
+            view.findNavController().navigate(action)
+        }
         val dateInString = args.productsell.updated_at
         if (dateInString != null) {
             val dateAfterCut = dateInString.substringBefore('T')
