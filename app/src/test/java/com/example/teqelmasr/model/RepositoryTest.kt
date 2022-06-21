5fd3c7bd0e73c4130a6e683af8c761adc7ca21b7
Ghada Mustafa <74993384+ghadaAwad99@users.noreply.github.com>
2022-06-21 18:53:55 +0200

    FakeDataSource Unit Testing

--- app/src/test/java/com/example/teqelmasr/model/RepositoryTest.kt
+++ app/src/test/java/com/example/teqelmasr/model/RepositoryTest.kt
@@ -4,38 +4,29 @@ import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import junit.framework.TestCase
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 
-@Config(sdk =[30])
+@Config(sdk = [30])
 @RunWith(AndroidJUnit4::class)
-class RepositoryTest : TestCase(){
+class RepositoryTest : TestCase() {
 
     private lateinit var repository: Repository
     private lateinit var fakeDataSource: FakeDataSource
 
     @Before
-    fun obtainRepoInstance(){
+    fun obtainRepoInstance() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         fakeDataSource = FakeDataSource()
         repository = Repository.getInstance(fakeDataSource, context)
     }
 
-    /*    @Before
-    fun createRepository() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        remoteDataSource = FakeRepository()
-        // Get a reference to the class under test
-        repo = Repository.getInstance(remoteDataSource, context)
-    }
-
     @Test
-    fun getOrders() = runBlockingTest {
-        // When tasks are requested from the tasks repository
-        val tasks = repo.getOrders("")
-
-        // Then tasks are loaded from the remote data source
-        assertEquals(2,tasks.orders.size)
-    }*/
+    fun getMyProducts() = runBlocking {
+        val tasks = repository.getMyProducts()
+        assertEquals(1, tasks.products?.size)
+    }
 }
